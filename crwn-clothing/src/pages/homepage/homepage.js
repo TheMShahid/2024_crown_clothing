4cad98de99cc214dbf1bb45d419ca6c4fecb1032
Muhammad Shahid Khan <shahidlanger0@gmail.com>
2024-10-25 12:37:36 +0500

    broken our homepage into smaller components, finished styling our menu-item component

---
+++ crwn-clothing/src/pages/homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Directory from "../../components/directory/directory";
+import "./homepage.scss";
+
+const HomePage = () => (
+  // main conponent of the home page
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
