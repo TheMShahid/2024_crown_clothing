941ad5171a628b9158fcb2cd79c1b66a9c266f94
Muhammad Shahid Khan <shahidlanger0@gmail.com>
2024-10-28 08:37:09 +0500

    updating our app with a shop page collection preview component and collection item component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,31 +1,13 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
 import { Route } from "react-router-dom";
-
-const HatsPage = () => (
-  // main component of the hats page
-  <div className="hats-page">
-    <h1>I am Hats Page but HTML Presentational Coponent</h1>
-  </div>
-);
-
-const JacketsPage = () => {
-  return (
-    // main component of the jackets page
-    <div className="jackets-page">
-      <h1>
-        I am jackets page but i am Javascript expression function like component
-      </h1>
-    </div>
-  );
-};
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/hats" component={HatsPage} />
-      <Route exact path="/jackets" component={JacketsPage} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
