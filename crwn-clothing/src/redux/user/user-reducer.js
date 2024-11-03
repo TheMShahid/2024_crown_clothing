6b1f8bb23bfd4dacff6c0e0cc65d5cb96309e661
Muhammad Shahid Khan <shahidlanger0@gmail.com>
2024-11-03 19:52:48 +0500

    updating our app to redirect to home if user is sgined in

--- crwn-clothing/src/redux/user/user-reducer.js
+++ crwn-clothing/src/redux/user/user-reducer.js
@@ -1,3 +1,5 @@
+import { UserActionTypes } from "./user.types";
+
 const INITIAL_STATE = {
   currentUser: null,
 };
@@ -6,7 +8,7 @@ const UserReducer = (state = INITIAL_STATE, action) => {
   // i can apply both the switch and if-else for better readibility
   // i have use the switch statement for better understanding of the code flow
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
