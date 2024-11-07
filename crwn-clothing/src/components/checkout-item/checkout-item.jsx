19d9c0eabead3142af40d30d178f4f65d1f230c1
Muhammad Shahid Khan <shahidlanger0@gmail.com>
2024-11-07 18:34:10 +0500

    added addItem and removeItem functionality to checkout component

--- crwn-clothing/src/components/checkout-item/checkout-item.jsx
+++ crwn-clothing/src/components/checkout-item/checkout-item.jsx
@@ -1,17 +1,43 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from "../../redux/cart/cart.actions";
 
 import "./checkout-item.scss";
 
-const CheckoutItem = ({ cartItem: { name, quantity, imageUrl, price } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img alt="item" src={imageUrl} />
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { name, quantity, imageUrl, price } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img alt="item" src={imageUrl} />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <span className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </span>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <span className="remove-button">&#10005;</span>
-  </div>
-);
+  );
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispath) => ({
+  clearItem: (item) => dispath(clearItemFromCart(item)),
+  addItem: (item) => dispath(addItem(item)),
+  removeItem: (item) => dispath(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
