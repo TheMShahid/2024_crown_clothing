4b8b49f64dac426ef89faa340dc07b08ab9f7035
Muhammad Shahid Khan <shahidlanger0@gmail.com>
2024-11-09 22:41:25 +0500

    moves our directory and shop data into its own reducer, and then created repective selectors and updated co rresponding components with new redux flow for directory/shop

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.css";
 
@@ -12,7 +13,9 @@ root.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>
